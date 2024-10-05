ba68fffb2b43770c646467fa1577aa5e680552b1
YoulonGG <johnyoulong@gmail.com>
2024-10-05 12:08:49 +0700

    Solve conflict

--- app/src/main/java/com/example/groceryapp/presentation/Login/PhoneNumberScreen.kt
+++ app/src/main/java/com/example/groceryapp/presentation/Login/PhoneNumberScreen.kt
@@ -3,7 +3,6 @@ package com.example.groceryapp.presentation.Login
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -27,13 +26,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.focus.FocusRequester
-import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -65,8 +61,7 @@ fun PhoneNumberScreen(navController: NavController) {
             painter = painterResource(id = R.drawable.baseline_arrow_back_ios_new_24),
             contentDescription = null,
             modifier = Modifier
-                .width(24.dp)
-                .height(24.dp)
+                .size(24.dp)
                 .clickable { navController.popBackStack() }
         )
         Spacer(modifier = Modifier.height(62.dp))
